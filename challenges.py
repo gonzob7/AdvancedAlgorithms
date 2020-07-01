887056e8c4734c9b2da77228d4daeb607d5ee18a
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-06-30 18:01:00 -0700

    rot oranges algo

--- challenges.py
+++ challenges.py
@@ -27,5 +27,35 @@ class Island_Solution:
         self.dfs(grid, i, j-1)
 
 islands = Island_Solution()
-print(sol.numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]))
-////
+print(islands.numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]))
+
+class Orange_Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        def createSet(grid, target):
+            result = set()
+            for y in range(len(grid)):
+                for x in range(len(grid[0])):
+                    if grid[y][x] == target:
+                        result.add((x,y))
+            return result
+        rotten = createSet(grid,2)
+        fresh = createSet(grid,1)
+
+        time = 0
+        while len(fresh) > 0:
+            turned = set()
+            for x,y in fresh:
+                #check all sides for rotten neihbors
+                if (x+1,y) in rotten or (x-1,y) in rotten or (x,y+1) in rotten or (x,y-1) in rotten:
+                    turned.add((x,y))
+            # print(time, rotten, fresh, turned)
+            if len(turned) == 0:
+                return -1
+            fresh.difference_update(turned)
+            rotten.update(turned)
+            time += 1
+
+        return time
+
+oranges = Orange_Solution()
+print(oranges.orangesRotting([[2,1,1],[1,1,0],[0,1,1]]))
